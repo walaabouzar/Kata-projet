c637d1458e1690ec35ff26990560a5a68331f67b
walaabouzar <bouzarwalaa@gmail.com>
2025-11-24 11:23:56 +0100

    Simplification de la méthode addMoney

--- src/main/java/re/forestier/edu/rpg/player.java
+++ src/main/java/re/forestier/edu/rpg/player.java
@@ -42,8 +42,7 @@ public class player {
         money = Integer.parseInt(money.toString()) - amount;
     }
     public void addMoney(int amount) {
-        var value = Integer.valueOf(amount);
-        money = money + (value != null ? value : 0);
+        money = money + amount;
     }
     public int retrieveLevel() {
         // (lvl-1) * 10 + round((lvl * xplvl-1)/4)
