e259604960d6d18f02b1325ea125545aaf52490e
Ikram Mokrani <ikrammokrani7@gmail.com>
2025-12-14 13:57:45 +0100

    Finalisation des tests de UpdatePlayer après mutation

--- src/test/java/re/forestier/edu/UpdatePlayerUnitTest.java
+++ src/test/java/re/forestier/edu/UpdatePlayerUnitTest.java
@@ -2,8 +2,11 @@ package re.forestier.edu;
 
 import org.junit.jupiter.api.*;
 import java.lang.reflect.Field;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Array;
 
+import re.forestier.edu.rpg.ObjectRewardService;
 import re.forestier.edu.rpg.UpdatePlayer;
 import re.forestier.edu.rpg.player;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,8 +22,6 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Random;
 
 public class UpdatePlayerUnitTest {
     @Test
@@ -49,7 +50,7 @@ public class UpdatePlayerUnitTest {
 
     }
     @Test
-    @DisplayName("addXp returns true on level-up")
+    @DisplayName("addXp retourne true on levelUp et l'ajout des abilities")
     void testAddXpLevelUp() {
         player p = new player("John", "Avatar", "ARCHER", 100, new ArrayList<>());
         int currentLevel = p.retrieveLevel();
@@ -79,17 +80,22 @@ public class UpdatePlayerUnitTest {
 
     }
     @Test
-    @DisplayName("on asure que ce random meme si on lui retrieve 0 + 0 reste meme")
+    @DisplayName("Verification de l'ajour d'un objet à partie de la liste des objets")
     void testRandomObjectAdded() {
         player p = new player("John", "Avatar", "DWARF", 100, new ArrayList<>());
 
         int initialSize = p.inventory.size();
         int currentLevel = p.retrieveLevel();
-        boolean result = UpdatePlayer.addXp(p, 200); // ça fera passer p.xp > 10 → lvl up
+        boolean result = UpdatePlayer.addXp(p, 200);
         int newLevel = p.retrieveLevel();
         assertTrue(newLevel > currentLevel); // le joueur a monté de niveau
-        assertTrue(result);                  // couvre le return true
+        assertTrue(result);       
         assertEquals(initialSize + 1, p.inventory.size());
+
+        //récupération de l'objet ajouté
+        String addedObject = p.inventory.get(p.inventory.size() - 1);
+        assertNotNull(addedObject);
+        assertFalse(addedObject.isBlank());
     }
 
 
@@ -106,6 +112,25 @@ public class UpdatePlayerUnitTest {
         // Vérifie que currenthealthpoints reste à 0
         assertEquals(0, p.currenthealthpoints);
     } 
+    @Test
+    public void testMajFinDeTourPrintsKoMessage() {
+        player p1 = new player("John", "Avatar", "ARCHER", 100, new ArrayList<>());
+        p1.currenthealthpoints = 0;
+
+        // Capture console
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out; // sauvegarde
+
+        System.setOut(new PrintStream(outContent));
+
+        try {
+            UpdatePlayer.majFinDeTour(p1);
+        } finally {
+            System.setOut(originalOut); // restaure proprement
+        }
+
+        assertTrue(outContent.toString().contains("Le joueur est KO !"));
+    }
     @Test
     void TestMajFinDeTourArcherCPLessHalfHP() {
     
@@ -124,7 +149,7 @@ public class UpdatePlayerUnitTest {
 
     }
     @Test
-    @DisplayName("test if  player is ko should not change health")
+    @DisplayName("test d'ajout de currenthealthpoints à DWARF quand il possède Holy Elixir")
     void TestMajFinDeTourCPLessHalfHP() {
         
         player p = new player("John", "Avatar", "DWARF", 100, new ArrayList<>());
@@ -141,7 +166,7 @@ public class UpdatePlayerUnitTest {
 
     }
     @Test
-    @DisplayName("test if  player is ko should not change health")
+    @DisplayName("test d'ajout de currenthealthpoints à ARCHER quand il ne possède pas Magic Bow")
     void TestMajFinDeTourARCHER() {
         
         player p = new player("John", "Avatar", "ARCHER", 100, new ArrayList<>());
@@ -149,14 +174,14 @@ public class UpdatePlayerUnitTest {
 
         p.healthpoints = 100;
         p.currenthealthpoints = 49; // statut KO
-        assertFalse(p.inventory.contains("Magic Bow"), "L'inventaire ne doit pas contenir Magic Bow au départ");        UpdatePlayer.majFinDeTour(p);
-        
+        assertFalse(p.inventory.contains("Magic Bow"), "L'inventaire ne doit pas avoir Magic Bow");        
+        UpdatePlayer.majFinDeTour(p);
         assertEquals(50, p.currenthealthpoints);
 
 
     }
     @Test
-    @DisplayName("test if  player is ko should not change health")
+    @DisplayName("test d'ajout de currenthealthpoints à ARCHER quand il possède Magic Bow")
     void TestMajFinDeTourARCHERMagicbow() {
         
         player p = new player("John", "Avatar", "ARCHER", 100, new ArrayList<>());
@@ -171,6 +196,7 @@ public class UpdatePlayerUnitTest {
 
     }
     @Test
+    @DisplayName("test d'ajout de currenthealthpoints à ADVENTURER quand level sup à 3")
     void TestMajFinDeTourADVENTURER() {
         
         player p = new player("John", "Avatar", "ADVENTURER", 100, new ArrayList<>());
@@ -185,7 +211,7 @@ public class UpdatePlayerUnitTest {
 
     }
     @Test
-    @DisplayName("test if  player is ko should not change health")
+    @DisplayName("test d'ajout de currenthealthpoints à ADVENTURER quand level inf à 3")
     void TestMajFinDeTourADVENTURERLevel3() {
         
         player p = new player("John", "Avatar", "ADVENTURER", 100, new ArrayList<>());
@@ -197,7 +223,7 @@ public class UpdatePlayerUnitTest {
     }
 
     @Test
-    @DisplayName("test if  player is ko should not change health")
+    @DisplayName("test currenthealthpoints sup à healthpoints")
     void TestMajFinDeTourSupOuEgale() {
         
         player p = new player("John", "Avatar", "DWARF", 100, new ArrayList<>());
@@ -209,7 +235,7 @@ public class UpdatePlayerUnitTest {
         assertEquals(50, p.currenthealthpoints);
     }
     @Test
-    @DisplayName("test if  player is ko should not change health")
+    @DisplayName("test currenthealthpoints inf à healthpoints ")
     void TestMajEgal() {
         
         player p = new player("John", "Avatar", "DWARF", 100, new ArrayList<>());
@@ -219,11 +245,34 @@ public class UpdatePlayerUnitTest {
 
         UpdatePlayer.majFinDeTour(p);
         assertEquals(50, p.currenthealthpoints);
-        
-
-
-
     }
+    @Test
+    @DisplayName("test currenthealthpoints egal healthpoints")
+    void TestMajEgalCurrent_w_Health() {
+        
+        player p = new player("John", "Avatar", "DWARF", 100, new ArrayList<>());
+
+        p.healthpoints = 50;
+        p.currenthealthpoints = 50; // statut KO 
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(50, p.currenthealthpoints);
+        assertEquals(50, p.healthpoints);
+    }
+    @Test
+    @DisplayName("test currenthealthpoints sup à healthpoints/2 mais inf à healthpoints")
+    void TestMajCurrentInfHealth() {
+        
+        player p = new player("John", "Avatar", "ADVENTURER", 100, new ArrayList<>());
+
+        p.healthpoints = 200;
+        p.currenthealthpoints = 120; // statut KO 
+        UpdatePlayer.majFinDeTour(p);
+        assertEquals(200, p.healthpoints);
+        assertEquals(120, p.currenthealthpoints);
+    }
+    
+
+
 
     }
 
